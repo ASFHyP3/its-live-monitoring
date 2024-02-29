c82c8a06b68874105d7998a606fef10776015ecb
Joseph H Kennedy <me@jhkennedy.org>
2024-02-29 14:25:23 -0900

    fix ruff's complaints

--- landsat/src/main.py
+++ landsat/src/main.py
@@ -1,11 +1,21 @@
+"""Lambda function to trigger low-latency Landsat processing from newly acquired scenes."""
 import json
 
 
-def process_scene(scene):
+def process_scene(scene: str) -> None:
+    """Process a Landsat scene."""
     print(scene)
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: dict, context: object) -> None:
+    """Landsat processing lambda function.
+
+    Accepts an event with SQS records for newly ingested Landsat scenes and processes each scene.
+
+    Args:
+        event: The event dictionary that contains the parameters sent when this function is invoked.
+        context: The context in which is function is called.
+    """
     for record in event['Records']:
         body = json.loads(record['body'])
         message = json.loads(body['Message'])
