c678537363b65dfeaef3c0ea81d4bdb80ef24106
jacquelynsmale <jacquelyn.smale@gmail.com>
2024-04-03 16:41:38 -0800

    working version of the update

--- status-messages/src/mattermost-update.py
+++ status-messages/src/mattermost-update.py
@@ -3,24 +3,21 @@ import logging
 import os
 import sys
 from datetime import datetime
-from pathlib import Path
 
 import boto3
 from mattermostdriver import Driver
 
 
-def get_itslive_queue():
-    os.environ['AWS_PROFILE'] = 'hyp3-its-live'
+def get_queue_status(queue_url: str) -> str:
     client = boto3.client('sqs')
     result = client.get_queue_attributes(
-        QueueUrl='https://sqs.us-west-2.amazonaws.com/986442313181/its-live-monitoring-prod-DeadLetterQueue'
-                 '-LjzW63l95LAP',
+        QueueUrl=queue_url,
         AttributeNames=['All'],
     )
     return result['Attributes']['ApproximateNumberOfMessages']
 
 
-def post(markdown_file: Path, channel: str = 'APD'):
+def post(channel: str = 'measures-its_live'):
     mattermost = Driver({
         'url': 'chat.asf.alaska.edu',
         'token': os.environ.get('MATTERMOST_PAT'),
@@ -32,7 +29,8 @@ def post(markdown_file: Path, channel: str = 'APD'):
 
     channel_info = mattermost.channels.get_channel_by_name_and_team_name('asf', channel)
 
-    dead_letter_queue_count = get_itslive_queue()
+    queue_url = 'https://sqs.us-west-2.amazonaws.com/986442313181/its-live-monitoring-prod-DeadLetterQueue-LjzW63l95LAP'
+    dead_letter_queue_count = get_queue_status(queue_url)
     mattermost_message = (f'Dead Letter Queue Count as of {datetime.now()} has '
                           f'{dead_letter_queue_count} entries')
 
@@ -52,8 +50,7 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument('markdown_file', type=Path, help='Markdown file with the post content')
-    parser.add_argument('--channel', default='tools-team', help='The MatterMost channel to post to')
+    parser.add_argument('--channel', default='measures-its_live', help='The MatterMost channel to post to')
 
     args = parser.parse_args()
 
@@ -64,3 +61,7 @@ def main():
     logging.basicConfig(format='%(message)s', level=logging.INFO, handlers=(out, err))
 
     post(**args.__dict__)
+
+
+if __name__ == '__main__':
+    main()
