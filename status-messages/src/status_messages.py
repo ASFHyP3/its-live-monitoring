298bc9c1d42ee21a7a537fb6290cc725d3b20c7f
Joseph H Kennedy <me@jhkennedy.org>
2024-04-08 12:46:10 -0800

    setup logging

--- status-messages/src/status_messages.py
+++ status-messages/src/status_messages.py
@@ -1,6 +1,9 @@
 """Lambda function to trigger Mattermost updates for Dead Letter Queue."""
 
+import argparse
+import logging
 import os
+import sys
 from datetime import datetime, timezone
 from pathlib import Path
 
@@ -14,6 +17,8 @@ MATTERMOST_PAT = os.environ['MATTERMOST_PAT']
 # You can find the ID for a channel by looking in the channel info
 MATTERMOST_CHANNEL_ID = 'mmffdcqsafdg8xyr747scyuqnw'  # ~measures-its_live
 
+log = logging.getLogger(__name__)
+log.setLevel(os.environ.get('LOGGING_LEVEL', 'INFO'))
 
 def get_queue_count() -> str:
     """Retrieve the message count of the Dead Letter Queue.
@@ -41,7 +46,7 @@ def lambda_handler(event: dict, context: dict) -> None:
     """
     mattermost = Driver({'url': 'chat.asf.alaska.edu', 'token': MATTERMOST_PAT, 'scheme': 'https', 'port': 443})
     response = mattermost.login()
-    print(response)
+    log.debug(response)
 
     dead_letter_queue_count = int(get_queue_count())
 
@@ -56,10 +61,30 @@ def lambda_handler(event: dict, context: dict) -> None:
         f'{dead_letter_queue_count} entries on {datetime.now(tz=timezone.utc).isoformat()}'
     )
 
+    log.info(f'Posting: "{mattermost_message}" to {MATTERMOST_CHANNEL_ID}')
     response = mattermost.posts.create_post(
         options={
             'channel_id': MATTERMOST_CHANNEL_ID,
             'message': mattermost_message,
         }
     )
-    print(response)
+    log.debug(response)
+
+
+def main() -> None:
+    """Command Line wrapper around `lambda_handler`."""
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Turn on verbose logging')
+    args = parser.parse_args()
+
+    logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    if args.verbose:
+        log.setLevel(logging.DEBUG)
+
+    log.debug(' '.join(sys.argv))
+    lambda_handler({}, {})
+
+
+if __name__ == '__main__':
+    main()
