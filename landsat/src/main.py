e5d89403b82eece2643afe20eb5eef3df8ca5459
Joseph H Kennedy <me@jhkennedy.org>
2024-04-15 13:43:46 -0800

    Log number of secondary scenes found

--- landsat/src/main.py
+++ landsat/src/main.py
@@ -102,6 +102,7 @@ def get_landsat_pairs_for_reference_scene(
 
     items = [item for page in results.pages() for item in page if _qualifies_for_processing(item, max_cloud_cover)]
 
+    log.debug(f'Found {len(items)} secondary scenes for {reference.id}')
     if len(items) == 0:
         return gpd.GeoDataFrame()
 
