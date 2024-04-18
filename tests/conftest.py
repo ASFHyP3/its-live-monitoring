6a0c6f75b9cf244677b8f97280cb5d2b89f3b637
Joseph H Kennedy <me@jhkennedy.org>
2024-04-17 21:26:19 -0800

    extend ruff ignores; add some typehints

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ def pystac_item_factory():
             datetime: dt.datetime,
             properties: dict,
             collection: str,
-    ):
+    ) -> pystac.item.Item:
         expected_item = pystac.item.Item(
             id=id,
             geometry=None,
@@ -29,13 +29,13 @@ def pystac_item_factory():
 
 @pytest.fixture
 def hyp3_job_factory():
-    def create_hyp3_job(granules: list):
+    def create_hyp3_job(granules: list) -> sdk.Job:
         return NonCallableMock(job_parameters={'granules': granules})
     return create_hyp3_job
 
 
 @pytest.fixture
 def hyp3_batch_factory(hyp3_job_factory):
-    def create_hyp3_batch(granules_list: list):
+    def create_hyp3_batch(granules_list: list) -> sdk.Batch:
         return sdk.Batch([hyp3_job_factory(granules) for granules in granules_list])
     return create_hyp3_batch
