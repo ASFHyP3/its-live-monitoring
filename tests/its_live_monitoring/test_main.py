12c49da3edcee7768706593bb16eb242cd006fe5
Andrew Player <andrewplayer3@gmail.com>
2024-04-22 15:00:55 -0500

    restructuring tests

---
+++ tests/its_live_monitoring/test_main.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+from dateutil.tz import tzutc
+from unittest.mock import MagicMock, patch
+
+import geopandas as gpd
+import hyp3_sdk as sdk
+
+from its_live_monitoring.src import main
+
+
+def test_get_landsat_stac_item(pystac_item_factory):
+    scene = 'LC08_L1TP_138041_20240128_20240207_02_T1'
+    properties = {
+        'instruments': ['OLI'],
+        'landsat:collection_category': 'T1',
+        'landsat:wrs_path': '001',
+        'landsat:wrs_row': '005',
+        'landsat:cloud_cover_land': 50,
+        'view:off_nadir': 0,
+    }
+    collection = 'landsat-c2l1'
+    expected_item = pystac_item_factory(id=scene, datetime=datetime.now(), properties=properties, collection=collection)
+
+    with patch('main.LANDSAT_CATALOG', MagicMock()):
+        main.LANDSAT_CATALOG.get_collection().get_item.return_value = expected_item
+        item = main._get_stac_item(scene)
+
+    assert item.collection_id == collection
+    assert item.properties == properties
+
+
+def test_get_sentinel2_stac_item(pystac_item_factory):
+    scene = 'S2B_13CES_20200315_0_L1C'
+    properties = {
+        'created': '2022-11-06T07:09:52.078Z',
+        'instruments': ['msi'],
+        'eo:cloud_cover': 28.1884,
+        'mgrs:utm_zone': 13,
+        'mgrs:latitude_band': 'C',
+        'mgrs:grid_square': 'ES',
+        's2:product_uri': '2B_MSIL1C_20200315T152259_N0209_R039_T13CES_20200315T181115.SAFE',
+    }
+    collection = 'sentinel-2-l1c'
+    dt = datetime.datetime(2020, 3, 15, 15, 24, 29, 455000, tzinfo=tzutc())
+    ref_scene = pystac_item_factory(id=scene, datetime=dt, properties=properties, collection=collection)
+    
+    sec_scenes = [
+        ''
+    ]
+
+    assert item.collection_id == expected_item.collection_id
+    assert item.properties['instruments'] == expected_item.properties['instruments']
+    assert item.properties['created'] == expected_item.properties['created']
+    assert item.properties['mgrs:utm_zone'] == expected_item.properties['mgrs:utm_zone']
+    assert item.properties['mgrs:latitude_band'] == expected_item.properties['mgrs:latitude_band']
+    assert item.properties['mgrs:grid_square'] == expected_item.properties['mgrs:grid_square']
+    assert item.properties['eo:cloud_cover'] == expected_item.properties['eo:cloud_cover']
+
+
+def test_deduplicate_hyp3_pairs(hyp3_batch_factory):
+    sec_scenes = [
+        'LC09_L1TP_138041_20240120_20240120_02_T1',
+        'LC08_L1TP_138041_20240112_20240123_02_T1',
+        'LC09_L1TP_138041_20240104_20240104_02_T1',
+    ]
+    ref_scenes = ['LC08_L1TP_138041_20240128_20240207_02_T1'] * 3
+    ref_acquisitions = ['2024-01-28T04:29:49.361022Z'] * 3
+
+    landsat_pairs = gpd.GeoDataFrame(
+        {'reference': ref_scenes, 'secondary': sec_scenes, 'reference_acquisition': ref_acquisitions}
+    )
+
+    with patch('main.HYP3.find_jobs', MagicMock(return_value=sdk.Batch())):
+        pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
+
+    assert pairs.equals(landsat_pairs)
+
+    landsat_jobs = hyp3_batch_factory(zip(ref_scenes, sec_scenes))
+    with patch('main.HYP3.find_jobs', MagicMock(return_value=landsat_jobs)):
+        pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
+
+    assert len(pairs) == 0
+
+    landsat_jobs = hyp3_batch_factory(zip(ref_scenes[:-1], sec_scenes[:-1]))
+    with patch('main.HYP3.find_jobs', MagicMock(return_value=landsat_jobs)):
+        pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
+
+    assert len(pairs) == 1
+
+
+def test_submit_pairs_for_processing(hyp3_batch_factory):
+    sec_scenes = [
+        'LC09_L1TP_138041_20240120_20240120_02_T1',
+        'LC08_L1TP_138041_20240112_20240123_02_T1',
+        'LC09_L1TP_138041_20240104_20240104_02_T1',
+    ]
+    ref_scenes = ['LC08_L1TP_138041_20240128_20240207_02_T1'] * 3
+
+    landsat_jobs = hyp3_batch_factory(zip(ref_scenes, sec_scenes))
+    landsat_pairs = gpd.GeoDataFrame({'reference': ref_scenes, 'secondary': sec_scenes})
+
+    with patch('main.HYP3.submit_prepared_jobs', MagicMock(return_value=landsat_jobs)):
+        jobs = main.submit_pairs_for_processing(landsat_pairs)
+
+    assert jobs == landsat_jobs
---
+++ tests/its_live_monitoring/test_sentinel2.py
@@ -0,0 +1,104 @@
+from copy import deepcopy
+import datetime
+import json
+import unittest.mock
+from pathlib import Path
+from unittest.mock import MagicMock
+
+import geopandas as gpd
+import hyp3_sdk as sdk
+import pystac
+from dateutil.tz import tzutc
+
+from its_live_monitoring.src import sentinel2
+
+
+SENTINEL2_CATALOG_real = sentinel2.SENTINEL2_CATALOG
+HYP3_real = sentinel2.HYP3
+
+SAMPLE_PAIRS = gpd.read_parquet('tests/data/sentinel2/S2B_13CES_20200315_0_L1C_pairs.parquet')
+
+
+def get_mock_pystac_item() -> unittest.mock.NonCallableMagicMock:
+    item = unittest.mock.NonCallableMagicMock()
+    item.collection_id = 'sentinel-2-l1c'
+    item.properties = {
+        'instruments': ['msi'],
+        'mgrs:utm_zone': '19',
+        'mgrs:latitude_band': 'D',
+        'mgrs:grid_square': 'EE',
+        'eo:cloud_cover': 30,
+    }
+    return item
+
+
+def test_qualifies_for_processing():
+    item = get_mock_pystac_item()
+    assert sentinel2._qualifies_for_processing(item)
+
+    item = get_mock_pystac_item()
+    item.collection_id = 'foo'
+    assert not sentinel2._qualifies_for_processing(item)
+
+    item = get_mock_pystac_item()
+    item.properties['instruments'] = ['mis']
+    assert not sentinel2._qualifies_for_processing(item)
+
+    item = get_mock_pystac_item()
+    item.properties['mgrs:utm_zone'] = '30'
+    item.properties['mgrs:latitude_band'] = 'B'
+    item.properties['mgrs:grid_square'] = 'ZZ'
+    assert not sentinel2._qualifies_for_processing(item)
+
+    item = get_mock_pystac_item()
+    item.properties['eo:cloud_cover'] = 75
+    assert not sentinel2._qualifies_for_processing(item)
+
+    item = get_mock_pystac_item()
+    item.properties['eo:cloud_cover'] = 0
+    assert sentinel2._qualifies_for_processing(item)
+
+    item = get_mock_pystac_item()
+    item.properties['eo:cloud_cover'] = -1
+    assert not sentinel2._qualifies_for_processing(item)
+
+
+def test_get_landsat_pairs_for_reference_scene(pystac_item_factory):
+    scene = 'S2B_13CES_20200315_0_L1C'
+    properties = {
+        'created': '2022-11-06T07:09:52.078Z',
+        'instruments': ['msi'],
+        'eo:cloud_cover': 28.1884,
+        'mgrs:utm_zone': 13,
+        'mgrs:latitude_band': 'C',
+        'mgrs:grid_square': 'ES',
+        's2:product_uri': '2B_MSIL1C_20200315T152259_N0209_R039_T13CES_20200315T181115.SAFE',
+    }
+    collection = 'sentinel-2-l1c'
+    dt = datetime.datetime(2020, 3, 15, 15, 24, 29, 455000, tzinfo=tzutc())
+    reference_item = pystac_item_factory(id=scene, datetime=dt, properties=properties, collection=collection)
+
+    sec_scenes = [
+        'S2B_13CES_20200224_0_L1C',
+        'S2B_13CES_20200211_0_L1C',
+        'S2B_13CES_20200201_0_L1C',
+    ]
+    sec_date_times = ['2020-02-24 15:24:28.312000+00:00', '2020-02-11 15:14:28.467000+00:00', '2020-02-01 15:14:26.405000+00:00']
+    sec_items = []
+    for scene, date_time in zip(sec_scenes, sec_date_times):
+        props = deepcopy(properties)
+        sec_items.append(pystac_item_factory(id=scene, datetime=date_time, properties=props, collection=collection))
+
+    with Path('tests/data/sentinel2/S2B_13CES_20200315_0_L1C_pages.json').open() as f:
+        pages_dict = json.load(f)
+        pages = (pystac.item_collection.ItemCollection.from_dict(page) for page in pages_dict)
+
+    sentinel2.SENTINEL2_CATALOG.search().pages.return_value = pages
+
+    df = sentinel2.get_sentinel2_pairs_for_reference_scene(reference_item)
+
+    assert (df['mgrs:utm_zone'] == reference_item.properties['mgrs:utm_zone']).all()
+    assert (df['mgrs:latitude_band'] == reference_item.properties['mgrs:latitude_band']).all()
+    assert (df['mgrs:grid_square'] == reference_item.properties['mgrs:grid_square']).all()
+    assert (df['instruments'].apply(lambda x: ''.join(x)) == ''.join(reference_item.properties['instruments'])).all()
+    assert (df['referenceId'] == reference_item.id).all()
