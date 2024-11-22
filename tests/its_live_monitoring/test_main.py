e6342bc9c24b0003cba67f867b36a2d92a6291f0
Andrew Player <andrewplayer3@gmail.com>
2024-11-22 14:46:52 -0500

    uncommented

--- tests/its_live_monitoring/test_main.py
+++ tests/its_live_monitoring/test_main.py
@@ -62,31 +62,31 @@ def test_get_key(mock_list_objects_v2):
     assert main.get_key(['N00E000', 'N00E010'], 'latest', 'earliest') == 'N00E010/earliest_X_latest_G0120V02_P000.nc'
 
 
-# @patch('main.HYP3.find_jobs')
-# def test_deduplicate_hyp3_pairs(mock_find_jobs, hyp3_batch_factory):
-#     sec_scenes = [
-#         'LC09_L1TP_138041_20240120_20240120_02_T1',
-#         'LC08_L1TP_138041_20240112_20240123_02_T1',
-#         'LC09_L1TP_138041_20240104_20240104_02_T1',
-#     ]
-#     ref_scenes = ['LC08_L1TP_138041_20240128_20240207_02_T1'] * 3
-#     ref_acquisitions = ['2024-01-28T04:29:49.361022Z'] * 3
+@patch('main.HYP3.find_jobs')
+def test_deduplicate_hyp3_pairs(mock_find_jobs, hyp3_batch_factory):
+    sec_scenes = [
+        'LC09_L1TP_138041_20240120_20240120_02_T1',
+        'LC08_L1TP_138041_20240112_20240123_02_T1',
+        'LC09_L1TP_138041_20240104_20240104_02_T1',
+    ]
+    ref_scenes = ['LC08_L1TP_138041_20240128_20240207_02_T1'] * 3
+    ref_acquisitions = ['2024-01-28T04:29:49.361022Z'] * 3
 
-#     landsat_pairs = gpd.GeoDataFrame(
-#         {'reference': ref_scenes, 'secondary': sec_scenes, 'reference_acquisition': ref_acquisitions}
-#     )
+    landsat_pairs = gpd.GeoDataFrame(
+        {'reference': ref_scenes, 'secondary': sec_scenes, 'reference_acquisition': ref_acquisitions}
+    )
 
-#     mock_find_jobs.side_effect = [sdk.Batch(), sdk.Batch()]
-#     pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
-#     assert pairs.equals(landsat_pairs)
+    mock_find_jobs.side_effect = [sdk.Batch(), sdk.Batch()]
+    pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
+    assert pairs.equals(landsat_pairs)
 
-#     mock_find_jobs.side_effect = [hyp3_batch_factory(zip(ref_scenes, sec_scenes)), sdk.Batch()]
-#     pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
-#     assert len(pairs) == 0
+    mock_find_jobs.side_effect = [hyp3_batch_factory(zip(ref_scenes, sec_scenes)), sdk.Batch()]
+    pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
+    assert len(pairs) == 0
 
-#     mock_find_jobs.side_effect = [hyp3_batch_factory(zip(ref_scenes[:-1], sec_scenes[:-1])), sdk.Batch()]
-#     pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
-#     assert len(pairs) == 1
+    mock_find_jobs.side_effect = [hyp3_batch_factory(zip(ref_scenes[:-1], sec_scenes[:-1])), sdk.Batch()]
+    pairs = main.deduplicate_hyp3_pairs(landsat_pairs)
+    assert len(pairs) == 1
 
 
 @patch('main.get_key')
