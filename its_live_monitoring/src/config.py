a0cafc1be058c4f1270e87515c4d9a798b51b67f
Joseph H Kennedy <me@jhkennedy.org>
2026-03-08 14:50:53 -0800

    grrr errant tuples

--- its_live_monitoring/src/config.py
+++ its_live_monitoring/src/config.py
@@ -16,13 +16,13 @@ LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO')
 
 # needed to deduplicate via HyP3
 HYP3_JOBS_TABLE_NAME = os.environ.get('JOBS_TABLE_NAME', '')
-EARTHDATA_USERNAME = username = (os.environ.get('EARTHDATA_USERNAME'),)
+EARTHDATA_USERNAME = os.environ.get('EARTHDATA_USERNAME')
 
 # needed to deduplicate via STAC
 STAC_ITEMS_ENDPOINT = _nullable_str(os.environ.get('STAC_ITEMS_ENDPOINT', 'None'))
 
 # needed to submit HyP3 jobs
 HYP3_API = os.environ.get('HYP3_API', '')
-EARTHDATA_PASSWORD = password = (os.environ.get('EARTHDATA_PASSWORD'),)
+EARTHDATA_PASSWORD = os.environ.get('EARTHDATA_PASSWORD')
 STAC_EXISTS_OK = _string_is_true(os.environ.get('STAC_EXISTS_OK', 'False'))
 PUBLISH_BUCKET = _nullable_str(os.environ.get('PUBLISH_BUCKET', 'None'))
